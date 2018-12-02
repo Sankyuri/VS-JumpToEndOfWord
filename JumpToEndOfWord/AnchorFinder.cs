d07fe3b53ed8e7b9405481c6cd412b41773cba5c
Stefan Eickelberg <stifu@codecreator.net>
2018-12-02 22:49:33 +0100

    Initial commit

---
+++ JumpToEndOfWord/AnchorFinder.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EnvDTE;
+using Microsoft;
+using Microsoft.VisualStudio.Shell;
+
+namespace JumpToEndOfWord
+{
+    public static class AnchorFinder
+    {
+        private static char[] charsToTrim = { ' ', '\t' };
+
+        public static int GetNumOfCharsToNextAnchor(VirtualPoint InitialPoint)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            if (InitialPoint.AtEndOfLine)
+                return 0;
+
+            EditPoint StartPoint = InitialPoint.CreateEditPoint();
+            string RightChar = StartPoint.GetText(1);
+
+            EditPoint EndPoint = InitialPoint.CreateEditPoint();
+            EndPoint.WordRight(1);
+
+            int iMoveCaretDistance = 0;
+            if (RightChar == " " || RightChar == "\t")
+            {
+                // Move to beginning of next word: Count whitespace
+                string text = StartPoint.GetText(EndPoint);
+                iMoveCaretDistance = text.Length;
+            }
+            else
+            {
+                // Move to end of current word: Trim whitespace
+                string text = StartPoint.GetText(EndPoint);
+                text = text.TrimEnd(charsToTrim);
+                iMoveCaretDistance = text.Length;
+            }
+
+            return iMoveCaretDistance;
+        }
+
+        public static int GetNumOfCharsToPreviousAnchor(VirtualPoint InitialPoint)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            if (InitialPoint.AtStartOfLine)
+                return 0;
+
+            EditPoint StartPoint = InitialPoint.CreateEditPoint();
+            StartPoint.WordLeft(1);
+
+            EditPoint EndPoint = InitialPoint.CreateEditPoint();
+            EndPoint.CharLeft(1);
+            string LeftChar = EndPoint.GetText(1);
+
+            int iMoveCaretDistance = 0;
+            if (LeftChar == " " || LeftChar == "\t")
+            {
+                // Move to end of previous word: Count whitespace
+                string text = StartPoint.GetText(EndPoint);
+                string textTrimmed = text.TrimEnd(charsToTrim);
+                iMoveCaretDistance = text.Length - textTrimmed.Length + 1; // +1 due to CharLeft of Endpoint
+            }
+            else
+            {
+                // Move to start of current word: Trim whitespace
+                string text = StartPoint.GetText(EndPoint);
+                iMoveCaretDistance = text.Length + 1; // +1 due to CharLeft of Endpoint
+            }
+
+            return iMoveCaretDistance;
+        }
+    }
+}
---
+++ JumpToEndOfWord/JumpToEndOfWord.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+using System.Linq;
+using EnvDTE;
+using Microsoft;
+
+namespace JumpToEndOfWord
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class JumpToEndOfWord
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("776724e3-a299-4f2a-9ee7-f1cc9dfe879e");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JumpToEndOfWord"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private JumpToEndOfWord(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static JumpToEndOfWord Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in JumpToEndOfWord's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new JumpToEndOfWord(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var dte = ServiceProvider.GetService(typeof(DTE)) as DTE;
+            if (dte == null)
+                return;
+            Document CurrentDoc = dte.ActiveDocument;
+            if (CurrentDoc == null)
+                return;
+            var textDocument = CurrentDoc.Object("TextDocument") as TextDocument;
+            if (textDocument == null)
+                return;
+
+            TextSelection selection = textDocument.Selection;
+            VirtualPoint point = selection.ActivePoint;
+
+            int iMoveCaretDistance = AnchorFinder.GetNumOfCharsToNextAnchor(point);
+            if (iMoveCaretDistance != 0)
+            {
+                if (!selection.IsEmpty)
+                    // When text was selected, the distance is too small by 1 for unknown reasons
+                    iMoveCaretDistance += 1;
+                selection.CharRight(false, iMoveCaretDistance);
+            }
+        }
+    }
+}
